ee35f62234cd0c5a13b9f6690f24bcd3b301d168
Hadi-Shah <snowismydestiny@gmail.com>
2024-09-08 23:02:39 +0530

    uploaded files

---
+++ Javascript/main.js
@@ -0,0 +1,15 @@
+// window.onload(()=>{
+//     document.getElementById('virat2').style.display = "none";
+// }
+// );
+
+
+window.onload = function() {
+    document.getElementById('content786').style.display = 'none';
+    // document.getElementById('marquee').style.display = 'none';
+    setTimeout(function() {
+        document.getElementById('loader786').style.display = 'none';
+        document.getElementById('content786').style.display = 'block';
+        // document.getElementById('marquee').style.display = 'block';
+    }, 3000); 
+};
